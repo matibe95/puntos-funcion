7d23e82fd7ef8ae15c981af4d9370bcedc1cae27
Matías Bellini <111000847+matibe95@users.noreply.github.com>
2023-08-13 14:14:53 -0300

    Update contactLinks.js

--- src/constants/contactLinks.js
+++ src/constants/contactLinks.js
@@ -4,11 +4,11 @@ export const contactLink = [
         title: 'Correo',
         link: 'mailto:parqueroosvelt.inc@gmail.com?Subject=Mensaje%20para%20equipo%20de%20Parque%20Roosevelt',
     },
-    {
-        id: 'footer-instagram',
-        title: 'Instagram',
-        link: 'www.instagram.com/',
-    },
+//    {
+//        id: 'footer-instagram',
+//        title: 'Instagram',
+//        link: 'www.instagram.com/',
+//    },
     {
         id: 'footer-github',
         title: 'Github',
