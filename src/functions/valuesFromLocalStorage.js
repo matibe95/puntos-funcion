da8f9a05551403aded73a5e3b478938507a8e8ab
matibe95 <matipro365@gmail.com>
2023-08-16 00:35:20 -0300

    create function to get all function points

--- src/functions/valuesFromLocalStorage.js
+++ src/functions/valuesFromLocalStorage.js
@@ -1,3 +1,5 @@
+import { TIPOS_FUNCIONES } from "../constants/puntos_funcion";
+
 export function getAllValues() {
     const { averagePF, title } = JSON.parse(localStorage.getItem("averagePF"));
     const allValues = {
@@ -10,3 +12,23 @@ export function getAllValues() {
     };
     return allValues;
   }
+
+export function getAllFunctionPoints () {
+  let arrayWithAllFunctionPoints = [];
+
+  TIPOS_FUNCIONES.forEach(element => {
+    const keyName = 'All_' + element.key
+    const elementoDeLocalStorage = JSON.parse(localStorage.getItem(keyName))
+    if (elementoDeLocalStorage !== null){
+      elementoDeLocalStorage.forEach(fp => {
+        const object = {
+          tipo: element.key,
+          name: fp.name,
+          value: fp.value
+        }
+        arrayWithAllFunctionPoints.push(object)
+      })
+    }
+  });
+  return arrayWithAllFunctionPoints
+}
